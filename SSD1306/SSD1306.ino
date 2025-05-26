b31f818c384005f76a43a6fc79d8643e8af51e22
Andrew Delgado <andrewdelgado017@gmail.com>
2025-05-26 14:44:43 -0700

    removed bear

--- SSD1306/SSD1306.ino
+++ SSD1306/SSD1306.ino
@@ -8,16 +8,13 @@
 //////// wifi
 const char* ssid     = "SSID";
 const char* password = "PASSWORD";
-
-
 HTTPClient http;
 WiFiClient client;
-// BearSSL::WiFiClientSecure client;
 ///////
 
 //////// display
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels // was 64
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 ////////
